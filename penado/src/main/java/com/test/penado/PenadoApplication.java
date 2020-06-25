e8f9447c161958100ea2d265c1a35f15edc9c82d
gendarmeria-springboot <66626431+gendarmeria-springboot@users.noreply.github.com>
2020-06-25 13:44:40 -0400

    Add files via upload
    25062020primera(subido por web,con error)

---
+++ penado/src/main/java/com/test/penado/PenadoApplication.java
@@ -0,0 +1,22 @@
+package com.test.penado;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableHystrix
+@EnableDiscoveryClient
+@SpringBootApplication
+
+
+public class PenadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenadoApplication.class, args);
+	}
+
+}
---
+++ penado/src/test/java/com/test/penado/PenadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.penado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
